5ff3b4c4c3d996c4870fc86d190f90362bd3418b
nguyennghia0108 <102567731+NguyenNghia0108@users.noreply.github.com>
2023-04-11 02:19:14 +0700

    add homepage

--- bkfood/src/components/header/index.js
+++ bkfood/src/components/header/index.js
@@ -1,11 +1,49 @@
-import './base.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
+import Logo from "../../assets/images/logo.png";
 
-function Header() {
-    return ( 
-        <div  className="header">
-            <p1>Header </p1> 
-        </div>
-    );
+function Header({ active }) {
+  return (
+    <>
+      <Navbar expand="lg" style={{ backgroundColor: "#D9D9D9" }} className="px-4">
+        <Container fluid>
+            <Navbar.Brand>
+                <Link to={"/home"}>
+                    <img src={Logo} alt="Logo" className="cursor-pointer" />
+                </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                <Nav className="ml-auto">
+                    <Nav.Item className="m-2 d-flex justify-content-center"> 
+                    <Button
+                        className="px-4 py-2 fw-bold header-button"
+                        href={"/login"}
+                        style={{ backgroundColor: "#A02F2F" }}
+                        onMouseOver={(e) => e.target.style.backgroundColor = "#6F2020"}
+                        onMouseOut={(e) => e.target.style.backgroundColor = "#A02F2F"}
+                    >
+                        LOGIN
+                    </Button>
+                    </Nav.Item>
+                    <Nav.Item className="m-2 d-flex justify-content-center">
+                    <Button
+                        className="px-4 py-2 fw-bold header-button"
+                        href={"/signup"}
+                        style={{ backgroundColor: "#A02F2F" }}
+                        onMouseOver={(e) => e.target.style.backgroundColor = "#6F2020"}
+                        onMouseOut={(e) => e.target.style.backgroundColor = "#A02F2F"}
+                    >
+                        SIGNUP
+                    </Button>
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default Header;
--- bkfood/src/pages/home/index.js
+++ bkfood/src/pages/home/index.js
@@ -1,12 +1,39 @@
-import Header from '../../components/header';
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import { Container, Button, Col, Row, Image } from "react-bootstrap";
+
+import Homepage from "../../assets/images/homepage.png";
 
 function Home() {
-    return ( 
-        <div>
-            <Header />
-            <h2>Home Page</h2> 
-        </div>
-    );
+  return (
+    <>
+      <Header />
+      <Container fluid style={{backgroundColor: "#1E1E1E", height: '81vh'}}>
+        <Row className="px-5 py-5">
+          <Col className="d-flex flex-column align-items-start justify-content-center px-4">
+            <p style={{ color: "#FF7E00", fontSize: "125px", fontWeight: 'bold' }}>BKSuggest</p>
+            <p className="my-5" style={{ color: "#FFFFFF", fontSize: "28px" }}>
+              Bạn đang gặp khó khăn trong quá trình lựa chọn địa điểm ăn uống?
+              Đừng lo, vì đã có BKSuggest đây rồi!
+            </p>
+            <Button
+              className="px-5 py-3 fw-bold header-button"
+              href={"/"}
+              style={{ backgroundColor: "#81BE00", borderRadius: '84px', fontSize: "20px" }}
+              onMouseOver={(e) => (e.target.style.backgroundColor = "#709523")}
+              onMouseOut={(e) => (e.target.style.backgroundColor = "#81BE00")}
+            >
+              CHỌN ĐỊA ĐIỂM NGAY
+            </Button>
+          </Col>
+          <Col className="d-flex align-items-start justify-content-center">
+            <Image src={Homepage} alt="Homepage" fluid/>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
