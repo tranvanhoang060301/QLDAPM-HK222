7e0c22e7d6c79eb094d2235deccc648b465f5bb5
Trần Văn Hoàng <vanhoang060301@gmail.com>
2023-04-11 22:02:18 +0700

    commit Search

--- bkfood/src/components/nav/index.js
+++ bkfood/src/components/nav/index.js
@@ -1,5 +1,16 @@
-function FavoritePlaces() {
-    return ( <h2>FavoritePlaces Page</h2> );
+import { Container, Form, Button, Row, Col, Alert } from "react-bootstrap";
+import './base.css';
+
+function Nav1() {
+    return ( 
+        <div className="nav_size">
+            <button type="button" class="btn btn-warning btnSize"><h2>Tìm kiếm quán ăn</h2></button>
+            <button type="button" class="btn btn-warning btnSize"><h2>Tìm kiếm quán nước</h2></button>
+            <button type="button" class="btn btn-warning btnSize"><h2>Gì cũng được</h2></button>
+            <button type="button" class="btn btn-warning btnSize"><h2>Địa điểm ưa thích</h2></button>
+        </div>
+
+    );
 }
 
-export default FavoritePlaces;
+export default Nav1;
--- bkfood/src/pages/search/index.js
+++ bkfood/src/pages/search/index.js
@@ -1,10 +1,23 @@
 import Header from '../../components/header';
+import Nav1 from '../../components/nav';
+import './base.css';
+import { TodoWrapper } from '../../components/wheel/TodoWrapper';
 
 function Search() {
     return (
         <div>
             <Header />
-            <h2>Search Page</h2>
+            <Nav1 />
+            <div className='wheel'>
+                <div className='wheel_around'>
+                    
+
+                </div>
+                <div className='wheel_around'>
+                    <TodoWrapper  />
+
+                </div>
+            </div>
         </div>
     );
 }
