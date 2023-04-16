d07b4aa24b7033b689c515baacf9a26eac8d9cf9
nguyennghia0108 <102567731+NguyenNghia0108@users.noreply.github.com>
2023-04-17 01:01:50 +0700

    add favorite card

--- bkvenue/src/pages/search/index.js
+++ bkvenue/src/pages/search/index.js
@@ -4,7 +4,9 @@ import Nav1 from "../../components/nav";
 import "./base.css";
 import Wheel from "../../components/wheel/wheel";
 import { TodoWrapper } from "../../components/wheel/TodoWrapper";
+import FavoriteCard from "../../components/favoriteCard";
 import { useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
 
 const foodArr = ["Hủ tiếu", "Cháo lòng", "Bún riêu", "Bún bò"];
 
@@ -32,13 +34,37 @@ function Search() {
       <div className="wheel">
         <div className="wheel_around">
           <div className="wheel_around1">
-            <Wheel option={optionList}/>
+            <Wheel option={optionList} />
           </div>
         </div>
         <div className="wheel_around">
           <TodoWrapper />
         </div>
       </div>
+      <Row className="d-flex justify-content-around flex-wrap m-5 px-5">
+        <h1 className="text-black mb-4">ĐỊA ĐIỂM GỢI Ý</h1>
+        <FavoriteCard
+          name="Bún bò Quán"
+          address="113, Tân Bình, TPHCM"
+          openingHours="7h-19h"
+          dish="Bún bò"
+          price="45-55k"
+        />
+        <FavoriteCard
+          name="Bún bò Quán"
+          address="113, Tân Bình, TPHCM"
+          openingHours="7h-19h"
+          dish="Bún bò"
+          price="45-55k"
+        />
+        <FavoriteCard
+          name="Bún bò Quán"
+          address="113, Tân Bình, TPHCM"
+          openingHours="7h-19h"
+          dish="Bún bò"
+          price="45-55k"
+        />
+      </Row>
     </div>
   );
 }
